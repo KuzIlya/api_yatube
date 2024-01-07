6415760347cd3c114967479fdf124e6643c81a4f
Кузнецов Илья <KuznetsovIlyaDM@yandex.ru>
2024-01-07 17:05:17 +0300

    1,5 version

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -18,14 +18,16 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly) 
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
